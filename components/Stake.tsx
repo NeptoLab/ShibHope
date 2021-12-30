e981c9e2bb894a28f1cf485aa2d5668e44186791
George Ivanov <george.ivanov@ecentria.com>
2021-12-30 14:30:24 -0600

    Flexible stake

--- components/Stake.tsx
+++ components/Stake.tsx
@@ -6,7 +6,7 @@ import Block from './Block';
 const Stake: React.FC<{ item: TStake, label: string }> = ({ item, label }) => {
   return (
     <Block p={4}>
-      <HStack alignItems="center" space={8}>
+      <HStack alignItems="center" space={8} flexWrap="wrap">
         <Text fontSize="20px" fontWeight={900} color="primary.500">{label}</Text>
         <Text>{item.owner}</Text>
         <Text fontWeight="bold">{item.value} GRUMPYSHIB</Text>
