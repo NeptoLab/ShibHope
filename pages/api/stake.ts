7603982b961a30f7b04efe7600d9be4c476e4d5a
George Ivanov <george.ivanov@ecentria.com>
2021-12-29 20:24:49 -0600

    Reimplement API

--- pages/api/stake.ts
+++ pages/api/stake.ts
@@ -4,13 +4,27 @@ import fetch from "isomorphic-fetch"
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Mutation_Root, Mutation_RootStake_CampaignArgs, Query_Root, StakeCampaignArgs } from "types/models";
 
-const STAKE_CAMPAIGN_MUTATION = `mutation stake_campaign($campaign_id: bigint!, $amount: numeric!, $text: String!, $tx_number: String!) {
-  insert_stake_one(object: {amount: $amount, comment: {data: {text: $text}}, campaign_id: $campaign_id, tx_number: $tx}) {
-    id
+const STAKE_CAMPAIGN_MUTATION = `
+  mutation stake_campaign($campaign_id: bigint!, $amount: numeric!, $text: String!, $tx_number: String!) {
+    insert_stake_one(object: {amount: $amount, comment: {data: {text: $text}}, campaign_id: $campaign_id, tx_number: $tx}) {
+      id
+    }
   }
-}`;
+`;
 
-const GET_CAMPAIGN_QUERY = ``;
+const GET_CAMPAIGN_QUERY = `
+  query get_campaign($id: bigint!) {
+    campaign_by_pk(id: $id) {
+      amount
+      category
+      created_at
+      description
+      id
+      location
+      owner
+    }
+  }
+`;
 
 const getCampaign = async (id: number) => {
   const response = await fetch(
