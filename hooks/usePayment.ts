7a03990646c34c5224aa47500a56dc64fdcb281d
George Ivanov <george.ivanov@ecentria.com>
2021-12-31 00:40:32 -0600

    Improve stability by removing bscscan API

--- hooks/usePayment.ts
+++ hooks/usePayment.ts
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import GrumpyShibaAbi from 'contracts/GrumpyShibaAbi.json';
 import useSWR from "swr";
 import type Web3 from "web3";
 import cookie from 'js-cookie';
@@ -9,16 +10,15 @@ const usePayment = (web3: Web3, account?: string | null) => {
   const contract = useRef<any>(null);
   const [ balance, setBalance ] = useState<number | undefined>(undefined);
   const { data: { data: { price } } = { data: { price: 0 } } } = useSWR('https://api.pancakeswap.info/api/v2/tokens/0xAe448cB5A3ec77BA4aDcc6C8f9621e5921DCd77a', fetcher);
-  const { data: { result, status } = { result: null, status: 0 } } = useSWR('https://api.bscscan.com/api?module=contract&action=getabi&address=0xae448cb5a3ec77ba4adcc6c8f9621e5921dcd77a', fetcher);
 
   useEffect(() => {
-    if (web3 && !!parseInt(status)) {
-      contract.current = new web3.eth.Contract(JSON.parse(result), '0xAe448cB5A3ec77BA4aDcc6C8f9621e5921DCd77a');
+    if (web3) {
+      contract.current = new web3.eth.Contract(GrumpyShibaAbi as any, '0xAe448cB5A3ec77BA4aDcc6C8f9621e5921DCd77a');
       contract.current.methods.balanceOf(account).call().then((balance: number) => {
         setBalance(balance);
       });
     }
-  }, [web3, result]);
+  }, [web3]);
 
   useEffect(() => {
     if (account && !cookie.get('token')) {
