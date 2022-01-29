543a3f69a76f1afd3866741b2268cb152fc46052
Georgii Ivanov <xercool90@gmail.com>
2022-01-28 23:37:11 -0600

    Fix layout height

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout: React.FC = ({ children }) => {
   return (
     <VStack flex={1} position="static">
       <Header />
-      <View maxW={[ '100%', '100%', '100%', '1170px' ]} mx={[4, 4, 4, "auto"]} flex={1} my={10}>
+      <View maxW={[ '100%', '100%', '100%', '1170px' ]} mx="auto" my={10}>
         {children}
       </View>
       <Footer />
