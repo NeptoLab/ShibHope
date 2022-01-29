55d4597b25f37a78194d3040a84a4e61857b03c9
Georgii Ivanov <xercool90@gmail.com>
2022-01-28 23:20:01 -0600

    Flex 1 for viewport

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC = ({ children }) => {
   return (
     <VStack flex={1} position="static">
       <Header />
-      <View maxW={[ '100%', '100%', '100%', '1170px' ]} mx="auto" my={10}>
+      <View maxW={[ '100%', '100%', '100%', '1170px' ]} flex={1} mx="auto" my={10}>
         {children}
       </View>
       <Footer />
