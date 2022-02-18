00555e855360ccb1a70ad8f062a33b162cc47f72
George Ivanov <george.ivanov@ecentria.com>
2022-02-17 23:49:05 -0600

    Bundle analyzer implementation

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 // @generated: @expo/next-adapter@3.1.10
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true' });
 const { withExpo } = require("@expo/next-adapter");
 const withFonts = require("next-fonts");
 const withImages = require('next-images');
 const withPlugins = require("next-compose-plugins");
+
 const withTM = require("next-transpile-modules")([
   "expo-font",
   "react-native-web",
@@ -20,6 +22,7 @@ module.exports = withPlugins(
     }}],
     withFonts,
     withExpo,
+    withBundleAnalyzer,
   ],
   {
     webpack5: true,
